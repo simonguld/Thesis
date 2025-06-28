bc7d260ead8a03516ef19b385397f6692260f131
Simon <guldager.simon@gmail.com>
2025-06-28 18:52:02 +0200

    visualization results for param variation

--- NematicAnalysis/na512exp.py
+++ NematicAnalysis/na512exp.py
@@ -23,7 +23,7 @@ def main():
     # STEP 1: Initialize the analysis parameters and data paths
     ##### ---------------------------------------------------
 
-    data_suffix='lbc' #'lambda_minus1'
+    data_suffix='lfric10bc' #'lambda_minus1'
     LL = 512
     mode = 'all' # 'all' or 'short'
 
@@ -65,8 +65,11 @@ def main():
 
     output_path = f'data\\na{LL}{data_suffix}'
     save_path = os.path.join(output_path, 'figs')
+    save_path0 = 'C:\\Users\\Simon Andersen\\OneDrive - University of Copenhagen\\PhD\\Active Nematic Defect Transition\\figs\\SI'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
+    if not os.path.exists(save_path0):
+        os.makedirs(save_path0)
 
     defect_list = []
 
@@ -174,18 +177,21 @@ def main():
         # STEP 4: Perform comparison analysis and plotting
         ##### ---------------------------------------------------
 
-        do_plotting_for = ['fig', 'fig2', 'fig3'] #'fig4']
+        do_plotting_for = ['fig', 'fig3'] #'fig4']
+        make_superfig = True
+        use_sus_in_superfig = False
 
-        #fig00, ax00 = plt.subplots(ncols=2, figsize=(10, 10/2.5))
-        #ax = ax00[0]
-        #ax2 = ax00[1]
+        if make_superfig: 
+            fig00, ax00 = plt.subplots(ncols=2, figsize=(10, 10/2.5))
+            ax = ax00[0]
+            ax3 = ax00[1]
 
         act_list_list = [ad.act_list[0], ad2.act_list_merged, ad.act_list[1]]
         marker_list = ['ro', 'bs', 'gd']
 
         ## plot av. defect density 
         if 'fig' in do_plotting_for or 'all' in do_plotting_for:
-            fig, ax = plt.subplots(figsize=(7,5))
+            if not make_superfig: fig, ax = plt.subplots(figsize=(7,5))
         
             av_defect_list = [ad.get_arrays_av(0)[-1], ad2.get_arrays_av(use_merged=True)[-1], ad.get_arrays_av(1)[-1]]
 
@@ -200,17 +206,26 @@ def main():
             ax.hlines(y=0, xmin=0, xmax=.052, linestyle='-', color='k', lw = 1 )
 
             ax.xaxis.set_minor_locator(ticker.MultipleLocator(2.5e-3))   
-            ax.yaxis.set_minor_locator(ticker.MultipleLocator(.5e-3))     
-            ax.set_xlim([.0178, 0.052])
-            ax.set_ylim(ymin=-.05e-2, ymax = .6e-2)
-            ax.legend(loc='upper center', ncols=3)
-            fig.savefig(os.path.join(save_path, 'av_density.png'), dpi=420, bbox_inches='tight')
+            ax.yaxis.set_minor_locator(ticker.MultipleLocator(.5e-3))  
+
+            if data_suffix == 'lbc':
+                act_min, act_max = 0.0082, 0.052
+            elif data_suffix == 'lk':
+                act_min, act_max = 0.0052, 0.052
+            else:
+                act_min, act_max = 0.0178, 0.052
+            ax.set_xlim([act_min, act_max])
+            ax.set_ylim(ymin=-.025e-2, ymax = .55e-2)
+            #ax.legend(loc='lower right', ncols=1)
+            if not make_superfig: 
+                ax.legend(loc='lower right', ncols=1)
+                fig.savefig(os.path.join(save_path, f'av_density_{data_suffix}.png'), dpi=420, bbox_inches='tight')
 
 
         ## plot susceptibility
         if 'fig2' in do_plotting_for or 'all' in do_plotting_for:
 
-            fig2, ax2 = plt.subplots(figsize=(7,5))
+            if not make_superfig: fig2, ax2 = plt.subplots(figsize=(7,5))
             sus_list = [ad.get_susceptibility(0), ad2.get_susceptibility(use_merged=True), ad.get_susceptibility(1)]
 
             for i, label in enumerate(label_list):
@@ -220,7 +235,16 @@ def main():
                 
             ax2.xaxis.set_minor_locator(ticker.MultipleLocator(2.5e-3))   
             ax2.yaxis.set_minor_locator(ticker.MultipleLocator(1))     
-            ax2.set_xlim([.0178, 0.052])
+
+            if data_suffix == 'lbc':
+                act_min, act_max = 0.0082, 0.052
+            elif data_suffix == 'lk':
+                act_min, act_max = 0.0052, 0.052
+            else:
+                act_min, act_max = 0.0178, 0.052
+            ax2.set_xlim([act_min, act_max])
+
+    
             ax2.set_ylim(ymin=0, ymax=13)
             ax2.set_xlabel(r'Activity ($\zeta$)')
             ax2.set_ylabel(r'Active susceptibility ($\overline{\chi_a})$')
@@ -228,15 +252,16 @@ def main():
 
             ax2.hlines(y=0, xmin=0, xmax=0.052, linestyle='-', color='k', lw = 1 )
             
-            ax2.legend(loc='upper right', ncols=1)
-            fig2.savefig(os.path.join(save_path, 'susceptibility.png'), dpi=420, bbox_inches='tight')
-
-        #fig00.savefig(os.path.join(save_path, 'av_density_susceptibility.png'), dpi=420, bbox_inches='tight', pad_inches=0.1)
+            if not make_superfig: 
+                ax2.legend(loc='upper right', ncols=1)
+                fig2.savefig(os.path.join(save_path, f'susceptibility_{data_suffix}.png'), dpi=420, bbox_inches='tight')
+            if use_sus_in_superfig:
+                fig00.savefig(os.path.join(save_path, f'av_density_susceptibility_{data_suffix}.png'), dpi=420, bbox_inches='tight', pad_inches=0.1)
 
         ## plot hyperuniformity exponent across and for no-slip using 3 approaches
         if 'fig3' in do_plotting_for or 'all' in do_plotting_for:
             
-            fig3, ax3 = plt.subplots(figsize=(7, 5))
+            if not make_superfig: fig3, ax3 = plt.subplots(figsize=(7, 5))
             fit_params_paths = [os.path.join(ad.output_paths[0], 'fit_params_sfac_time_av.npy'), 
                                 os.path.join(ad2.output_merged, 'fit_params_sfac_time_av.npy'),
                                 os.path.join(ad.output_paths[1], 'fit_params_sfac_time_av.npy')]
@@ -244,67 +269,41 @@ def main():
             for i, label in enumerate(label_list):
                 fit_params = np.load(fit_params_paths[i])
 
-              #  if label.startswith('No-'):
-               #     act_arr = np.array(act_list_list[i])
-                #    act_arr_mask = (act_arr != )
-
-            #   print(len(act_list_list[i]), fit_params.shape)
-                ax3.errorbar(act_list_list[i], fit_params[:,0], yerr=fit_params[:,2], fmt = marker_list[i], label=label,
-                            elinewidth=1.5, capsize=1.5, capthick=1, markersize = 5, alpha=.5)
+                act_idx_min = 0 if data_suffix == 'lbc' else 0
+                
+                ax3.errorbar(act_list_list[i][act_idx_min:], fit_params[act_idx_min:,0], yerr=fit_params[act_idx_min:,2], \
+                            fmt = marker_list[i], elinewidth=1.5, capsize=1.5, capthick=1, markersize = 5, \
+                            alpha=.5, label=None if make_superfig else label)
 
             ax3.xaxis.set_minor_locator(ticker.MultipleLocator(2.5e-3))   
-            ax3.yaxis.set_minor_locator(ticker.MultipleLocator(.1))     
-            ax3.set_xlim([.0182, 0.062])
+            ax3.yaxis.set_minor_locator(ticker.MultipleLocator(.1))
+
+            if data_suffix == 'lbc':
+                act_min, act_max = 0.0082, 0.052
+            elif data_suffix == 'lk':
+                act_min, act_max = 0.0052, 0.052
+            else:
+                act_min, act_max = 0.0178, 0.052
+            ax3.set_xlim([act_min, act_max])
+
             ax3.set_ylim(ymin=-.6, ymax=.3)
             ax3.set_xlabel(r'Activity ($\zeta$)')
             ax3.set_ylabel(r'Hyperuniformity exponent ($\gamma$)')
             ax3.vlines(x=0.022, ymin = -1.2, ymax=1.2, linestyle='--', color='k', lw = 1, alpha=.65, zorder=-10)
-         #   ax3.vlines(x=0.022, ymin = -.16, ymax=1.2, linestyle='--', color='k', lw = 1)
             ax3.hlines(y=0, xmin=0, xmax=0.092, linestyle='-', color='k', lw = 1 )
 
-            ax3.legend(loc='lower right', ncols=1)
-            fig3.savefig(os.path.join(save_path, 'alpha.png'), dpi=420, bbox_inches='tight', pad_inches=0.1)
-
-        ## plot sfac exponent for no-slip
-        if 'fig4' in do_plotting_for or 'all' in do_plotting_for:
-            fig4, ax4 = plt.subplots(figsize=(7, 5))
-            
-            label_list_no_slip = [r'$\overline{\delta \rho ^2}$ (time av. of fits)', 
-                                rf'$S(k)$ (fit of time av.)',
-                                rf'$S(k)$ (time av. of fits)']  
-            fit_params_paths_no_slip = [os.path.join(ad.output_paths[1], 'fit_params_count.npy'), 
-                                    os.path.join(ad.output_paths[1], 'fit_params_sfac_time_av.npy'),
-                                    os.path.join(ad.output_paths[1], 'alpha_list_sfac.npy')]
-
-            for i, label in enumerate(label_list_no_slip):
-                fit_params = np.load(fit_params_paths_no_slip[i])
-                ax4.errorbar(act_list_list[-1], fit_params[:,0], yerr=fit_params[:,1] if i==2 else fit_params[:,2]
-                            , fmt = marker_list[i], label=label, elinewidth=1.5, capsize=1.5, 
-                            capthick=1, markersize = 5, alpha=.5)
-            
-            ax4.xaxis.set_minor_locator(ticker.MultipleLocator(2e-3))
-            ax4.set_xlim([.018, 0.052])
-            ax4.set_xlabel(r'Activity ($\zeta$)')
-            ax4.set_ylabel(r'Hyperuniformity exponent ($\gamma$)')
-            ax4.set_ylim(ymin=-1., ymax=.8)
-            ax4.vlines(x=0.022, ymin = -1.2, ymax=1.2, linestyle='--', color='k', lw = 1)
-            ax4.hlines(y=0, xmin=0, xmax=0.052, linestyle='-', color='k', lw = 1 )
-            ax4.legend(loc='lower right', ncols=1)
-            fig4.savefig(os.path.join(save_path, 'alpha_no_slip.png'), dpi=420, bbox_inches='tight', pad_inches=0.1)
+            if not make_superfig:
+                ax3.legend(loc='lower right', ncols=1)
+                fig3.savefig(os.path.join(save_path, f'alpha_{data_suffix}.png'), dpi=420, bbox_inches='tight', pad_inches=0.1)
+        if make_superfig and not use_sus_in_superfig:
+            fig00.legend(ncol=3, fontsize = 16, bbox_to_anchor=(0.515, 1.04), loc='upper center')
+            fig00.savefig(os.path.join(save_path, f'av_density_alpha_{data_suffix}.png'), dpi=420, bbox_inches='tight', pad_inches=0.1)
+            fig00.savefig(os.path.join(save_path, f'av_density_alpha_{data_suffix}.eps'), bbox_inches='tight', pad_inches=0.1)
+            fig00.savefig(os.path.join(save_path0, f'av_density_alpha_{data_suffix}.png'), dpi=420, bbox_inches='tight', pad_inches=0.1)
+            fig00.savefig(os.path.join(save_path0, f'av_density_alpha_{data_suffix}.eps'), bbox_inches='tight', pad_inches=0.1)
 
         plt.close('all')
 
-        if 0:
-            fig0, ax0 = plt.subplots(figsize=(9, 6))
-            count_var_av = ad.get_arrays_av(-1)[1]
-            windows = ad.window_sizes[-1]
-
-            for j, act in enumerate(act_list_list[-1][:4]):
-                print(act, np.nansum(count_var_av[:,j,0])/ count_var_av.shape[0])
-                ax0.errorbar(windows, count_var_av[:, j, 0], count_var_av[:, j, 1], fmt='.', color=f'C{j}',
-                            label=f'Activity {act:.3f}', elinewidth=1.5, capsize=1.5, capthick=1, markersize=3, alpha=.5)
-            ax0.legend()
-            fig0.savefig(os.path.join(save_path, 'count_var_av_no_slip.png'), dpi=420, bbox_inches='tight', pad_inches=0.1)
 
 
 if __name__ == "__main__":
