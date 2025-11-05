fde689f1b62571b89cced927e8ebcaaf96733b1e
Simon <guldager.simon@gmail.com>
2025-11-05 20:12:27 +0100

    generalize CID classes to incorporate morton ordering

--- Universality/ComputableInformationDensity_dev/cid.py
+++ Universality/ComputableInformationDensity_dev/cid.py
@@ -2,9 +2,84 @@ import os
 from multiprocessing import cpu_count
 
 import numpy as np
-from .cid_abc import ComputableInformationDensity
+from .cid_abc import ComputableInformationDensity, ComputableInformationDensity_old
+from .hilbert_curve import hilbert_curve, precompute_hcurves, precompute_zcurves
+
 
 class CID(ComputableInformationDensity):
+    def __init__(self, dim, nshuff, nbits=None, data_shape=None,
+                ordering='hilbert', mode='lz77', verbose=False):
+
+        self.ordering = ordering
+
+        if ordering == 'hilbert':
+            self.__init_hilbert(dim, nbits)
+        elif ordering == 'zcurve':
+            self.__init_zcurve(dim, data_shape)
+        else:
+            raise ValueError("ordering must be 'hilbert' or 'zcurve'")
+
+        # CPU allocation depending on Slurm or local environment
+        slurm_cpus = os.getenv("SLURM_CPUS_PER_TASK")
+        available_cpus = int(slurm_cpus) if slurm_cpus else cpu_count()
+        self.ncpus = min(available_cpus, self.length)
+
+        super().__init__(dim, nshuff, mode=mode, verbose=verbose)
+
+    def __init_hilbert(self, dim, nbits):
+        if nbits is None:
+            raise ValueError("nbits must be provided for hilbert ordering")
+
+        self.nbits = nbits
+        self.size = 1 << self.nbits
+
+        # Precompute hilbert curves
+        self.hamiltonian_cycle = [0, 1, 0, 2, 1, 0, 1, 2]
+        self.principal_hcurve = hilbert_curve(dim, self.nbits)
+        self.length = len(self.hamiltonian_cycle)
+        self.curves = precompute_hcurves(
+            self.hamiltonian_cycle,
+            self.principal_hcurve,
+            self.nbits
+        )
+
+    def __init_zcurve(self, dim, data_shape):
+        if data_shape is None:
+            raise ValueError("data_shape must be provided for zcurve ordering")
+        if len(data_shape) != dim:
+            raise ValueError("data_shape length must match dim for zcurve ordering")
+
+        # Determine required bit-depth based on largest dimension
+        self.nbits = max((s - 1).bit_length() for s in data_shape)
+        self.size = 1 << self.nbits
+
+        if any(s > self.size for s in data_shape):
+            raise ValueError("all dimensions in data_shape must be at most 2**nbits for zcurve ordering")
+
+        self.length = np.math.factorial(dim)
+        self.curves = precompute_zcurves(data_shape, self.nbits)
+
+    def __hscan(self, data, hcurve):
+        """ Hilbert scaned view of data. """
+        return data[tuple( hcurve )].T.ravel()
+
+    def __zscan(self, data_flattened, zcurve):
+        """ zscan view of data. """
+        return data_flattened[zcurve]
+
+    def scan(self, data, curve):
+        if self.ordering == 'hilbert':
+            return self.__hscan(data, curve)
+        else:
+            return self.__zscan(data, curve)
+
+    def __call__(self, data):
+        cid_av, cid_std, cid_shuffle, cid_vals = super().calc_cid(data,)
+        cid_sem = cid_std / np.sqrt(self.length)
+        return cid_av, cid_sem, cid_shuffle, cid_vals
+
+
+class CID_old(ComputableInformationDensity_old):
     
     hamiltonian_cycle = [0, 1, 0, 2, 1, 0, 1, 2]
     length = len(hamiltonian_cycle)
@@ -16,52 +91,39 @@ class CID(ComputableInformationDensity):
         super().__init__(dim, nbits, nshuff, mode, verbose)
         if verbose:
             print(f"Using {self.ncpus} workers for CID calculations")
-
-    def itter_hscan2(self, data):
-        """ yields all 8 distinct Hilbert scanned views of the data. 
-        Since a view is returned, this operation is O(1). """
-        for k in self.hamiltonian_cycle:
-            hcurve = self.principal_hcurve  # view of principal_hcurve i.e. O(1)
-            if k == 0: hcurve[0] = (self.size - 1) - hcurve[0]
-            if k == 1: hcurve[1] = (self.size - 1) - hcurve[1]
-            if k == 2: hcurve[[0,1]] = hcurve[[1,0]]
-            yield self.hscan(data, hcurve)  # view of data, i.e. O(1)
-
-    def itter_hscan(self, data):
-        """Yield Hilbert scanned views using precomputed hcurves (no recomputation)."""
-        for hcurve in self.hcurves:
-            yield self.hscan(data, hcurve)
-        
-    def __call__(self, data):
-        cid_av, cid_std, cid_shuffle = super().__call__(data, n_workers=self.ncpus)
-        cid_sem = cid_std / np.sqrt(self.length)
-        return cid_av, cid_sem, cid_shuffle
     
+    def __call__(self, data):
+        cid_av, cid_std, cid_shuffle, cid_vals = super().__call__(data, n_workers=self.ncpus)
+        cid_sem = cid_std / np.sqrt(self.length)
+        return cid_av, cid_sem, cid_shuffle, cid_vals
+
     def __call2__(self, data):
         cid_av, cid_std, cid_shuffle = super().__call2__(data, n_workers=self.ncpus)
         cid_sem = cid_std / np.sqrt(self.length)
         return cid_av, cid_sem, cid_shuffle
 
-def cid2d(nbits, nshuff, mode='lz77', verbose=False):
+
+
+def cid2d(nshuff, nbits=None, data_shape=None, ordering='hilbert',mode='lz77', verbose=False):
     """ Two-dimensional CID Analysis \n
     Args:
         order: linear system size (log2) (int).
         nshuff: number of radnom shuffles of data.
     Returns: instance of the CID class """
-    return CID(2, nbits, nshuff, mode, verbose)
+    return CID(dim=2, nbits=nbits, data_shape=data_shape, nshuff=nshuff, ordering=ordering, mode=mode, verbose=verbose)
 
-def sequential_time(nbits, nshuff, mode='lz77', verbose=False):
+def sequential_time(nbits, nshuff, data_shape=None, ordering='hilbert', mode='lz77', verbose=False):
     """ Spatiotemporal CID Analysis \n
     Args:
         order: linear system size (log2) (int).
         nshuff: number of radnom shuffles of data.
     Returns: instance of the CID class """
-    return CID(2, nbits, nshuff, mode, verbose)
+    return CID(dim=2, nbits=nbits, data_shape=data_shape, nshuff=nshuff, ordering=ordering, mode=mode, verbose=verbose)
 
-def interlaced_time(nbits, nshuff, mode='lz77', verbose=False):
+def interlaced_time(nbits, nshuff, data_shape=None, ordering='hilbert', mode='lz77', verbose=False):
     """ Spatiotemporal CID Analysis \n
     Args:
         order: linear system size (log2) (int).
         nshuff: number of radnom shuffles of data.
     Returns: instance of the CID class """
-    return CID(3, nbits, nshuff, mode, verbose)
+    return CID(dim=3, nbits=nbits, data_shape=data_shape, nshuff=nshuff, ordering=ordering, mode=mode, verbose=verbose)
--- Universality/ComputableInformationDensity_dev/cid_abc.py
+++ Universality/ComputableInformationDensity_dev/cid_abc.py
@@ -1,11 +1,62 @@
 from .computable_information_density import cid, cid78, cid_linear, cid_hybrid, cid_shuffle
-from .hilbert_curve import hilbert_curve, precompute_hcurves, precompute_zcurves
+from .hilbert_curve import hilbert_curve, precompute_hcurves
 from abc import ABC, abstractmethod
 from multiprocessing import Pool
 import numpy as np
 import time
 
+
 class ComputableInformationDensity(ABC):
+
+    """ Abstract base class for the Computable Information Density """
+    def __init__(self, dim, nshuff, mode='lz77', verbose=False):
+        
+        self.size = 1 << self.nbits
+        self.nshuff = nshuff
+        self.dim = dim
+        self.mode = mode
+        self.verbose = verbose
+
+        # select CID implementation
+        cid_dict = {'lz77': cid,
+                'lz78': cid78,
+                    'linear': cid_linear,
+                    'hybrid': cid_hybrid}
+        self.cid = cid_dict[mode]
+    
+    @abstractmethod
+    def scan(self, data):
+        pass
+
+    def itter_scan(self, data):
+        """Yield scanned views using precomputed curves (no recomputation)."""
+        for curve in self.curves:
+            yield self.scan(data, curve)
+
+    def cid_shuffle(self, data):
+        """ CID of randomly shuffled data """
+        t1 = time.perf_counter()
+        cid_shuff_val = cid_shuffle(data.ravel(), self.nshuff, cid_mode=self.mode)
+        t2 = time.perf_counter()
+        if self.verbose: print(f"shuffling took {t2 - t1:.2f} seconds")
+        return cid_shuff_val
+    
+    def calc_cid(self, data,):
+        if self.ordering == 'hilbert':
+            data = np.transpose(data).reshape((-1, ) + (self.size, ) * self.dim).T
+        else:
+            data = data.ravel()
+        with Pool(self.ncpus) as pool:
+            cid_pool = pool.map_async(self.cid, self.itter_scan(data))
+            pool.close() 
+            pool.join()
+        cid_vals = cid_pool.get()
+        if self.verbose: print(cid_vals)
+        return np.mean(cid_vals), np.std(cid_vals, ddof=1), self.cid_shuffle(data), cid_vals
+
+
+class ComputableInformationDensity_old(ABC):
+
     """ Abstract base class for the Computable Information Density """
     def __init__(self, dim, nbits, nshuff, mode='lz77', verbose=False):
         
@@ -17,6 +68,7 @@ class ComputableInformationDensity(ABC):
 
         self.principal_hcurve = hilbert_curve(dim, nbits)
         self.hcurves = precompute_hcurves(self.hamiltonian_cycle, self.principal_hcurve, nbits)
+        #self.zcurves = precompute_zcurves( (self.size, ) * dim, nbits)
 
         # select CID implementation
         cid_dict = {'lz77': cid,
@@ -25,44 +77,45 @@ class ComputableInformationDensity(ABC):
                     'hybrid': cid_hybrid}
         self.cid = cid_dict[mode]
     
-    @abstractmethod
-    def itter_hscan(self, data): pass
+    def itter_hscan(self, data):
+        """Yield Hilbert scanned views using precomputed hcurves (no recomputation)."""
+        for hcurve in self.hcurves:
+            yield self.hscan(data, hcurve)
 
-    @abstractmethod
-    def itter_hscan2(self, data): pass
-
-    def precompute_hcurves(self):
-        """Precompute all hcurve coordinate variants exactly once."""
-        hcurves = []
-
-        h = self.principal_hcurve.copy()
-
-        for k in self.hamiltonian_cycle:
-            if k == 0: 
-                h[0] = (self.size - 1) - h[0]
-            if k == 1:
-                h[1] = (self.size - 1) - h[1]
-            if k == 2:
-                h[[0, 1]] = h[[1, 0]]
-
-            hcurves.append(h.copy())
-        return hcurves
+    def itter_zscan(self, data_flattened):
+        """Yield zscan views using precomputed zcurves (no recomputation)."""
+        for zcurve in self.zcurves:
+            yield self.zscan(data_flattened, zcurve)
 
     def hscan(self, data, hcurve):
         """ Hilbert scaned view of data. """
         return data[tuple( hcurve )].T.ravel()
-    
+
+    def zscan(self, data_flattened, zcurve):
+        """ zscan view of data. """
+        return data_flattened[zcurve]
+
     def cid_shuffle(self, data):
         """ CID of randomly shuffled data """
-        # flattened- and hilbert scanned- view are
-        # statistically equivalent after shuffling: 
         t1 = time.perf_counter()
         cid_shuff_val = cid_shuffle(data.ravel(), self.nshuff, cid_mode=self.mode)
         t2 = time.perf_counter()
         if self.verbose: print(f"shuffling took {t2 - t1:.2f} seconds")
         return cid_shuff_val
     
-    def __call__(self, data, n_workers):
+    def calc_zorder_cid(self, data, n_workers):
+        data_flattened = data.ravel()
+        t1 = time.perf_counter()
+        with Pool(n_workers) as pool:
+            cid_pool = pool.map_async(self.cid, self.itter_zscan(data_flattened))
+            pool.close() 
+            pool.join()
+        t2 = time.perf_counter()
+        if self.verbose: print(f"CID took {t2 - t1:.2f} seconds")
+        cid_vals = cid_pool.get()
+        return np.mean(cid_vals), np.std(cid_vals, ddof=1), self.cid_shuffle(data)
+    
+    def calc_horder_cid(self, data, n_workers):
         # reshape data to be compatible with Hilbert scan:
         data = np.transpose(data).reshape((-1, ) + (self.size, ) * self.dim).T
         # create and configure a pool of workers:
@@ -75,17 +128,18 @@ class ComputableInformationDensity(ABC):
         if self.verbose: print(f"CID took {t2 - t1:.2f} seconds")
         cid_vals = cid_pool.get()
         return np.mean(cid_vals), np.std(cid_vals, ddof=1), self.cid_shuffle(data)
-    
-    def __call2__(self, data, n_workers):
+
+    def __call__(self, data, n_workers):
         # reshape data to be compatible with Hilbert scan:
         data = np.transpose(data).reshape((-1, ) + (self.size, ) * self.dim).T
         # create and configure a pool of workers:
         t1 = time.perf_counter()
         with Pool(n_workers) as pool:
-            cid_pool = pool.map_async(self.cid, self.itter_hscan2(data))
+            cid_pool = pool.map_async(self.cid, self.itter_hscan(data))
             pool.close()    # close the process pool
             pool.join()     # wait for all workers to complete
         t2 = time.perf_counter()
         if self.verbose: print(f"CID took {t2 - t1:.2f} seconds")
         cid_vals = cid_pool.get()
-        return np.mean(cid_vals), np.std(cid_vals, ddof=1), self.cid_shuffle(data)
+        return np.mean(cid_vals), np.std(cid_vals, ddof=1), self.cid_shuffle(data), cid_vals
+    
